392a04f54558f25f40689d5a07771147b4b9f9b3
Sophia Freuden <sfreuden@.g.harvard.edu>
2019-11-18 23:41:21 -0500

    Date rendering of 2018 crime data file in scratch.R

---
+++ scratch.R
@@ -0,0 +1,34 @@
+x <- read_csv(file = "raw-data/CrimeData-2018.csv", col_types = cols(
+  Address = col_character(),
+  CaseNumber = col_character(),
+  CrimeAgainst = col_character(),
+  Neighborhood = col_character(),
+  OccurDate = col_character(),
+  OccurTime = col_character(),
+  OffenseCategory = col_character(),
+  OffenseType = col_character(),
+  OpenDataLat = col_double(),
+  OpenDataLon = col_double(),
+  OpenDataX = col_character(),
+  OpenDataY = col_character(),
+  ReportDate = col_character(),
+  OffenseCount = col_double()
+))
+
+sample <- x %>% sample_n(10)
+sample %>% mutate(OccurDate = mdy(OccurDate))
+
+x <- x %>%
+  mutate(OccurDate = mdy(OccurDate)) %>%
+  mutate(ReportDate = mdy(ReportDate))
+
+sample2 <- x %>% sample_n(10)
+sample3 <- sample2 %>% mutate(ReportYear = year(ReportDate))
+
+x <- x %>% mutate(OccurYear = year(OccurDate))
+x <- x %>% mutate(OccurMonth = month(OccurDate))
+
+monthspread <- x %>% group_by(OccurMonth) %>% count()
+
+ggplot(monthspread, aes(x = OccurMonth, y = n)) + geom_line()
+
