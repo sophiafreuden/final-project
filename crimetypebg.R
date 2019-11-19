54ae308872c66e921b7b505876445c1416fd5137
Sophia Freuden <sfreuden@.g.harvard.edu>
2019-11-19 16:51:13 -0500

    years added to individual crime files, filtered for person crimes only, coalecsed all filtered person crime files into one tibble

--- crimetypebg.R
+++ crimetypebg.R
@@ -85,3 +85,25 @@ crime19 <- read_csv(file = "raw-data/CrimeData-2019.csv", col_types = cols(
 ))
 
 ggplot(crime19, aes(x = OffenseCategory)) + geom_bar() + coord_flip()
+
+crime15$Year <- 2015
+
+crime16$Year <- 2016
+
+crime17$Year <- 2017
+
+crime18$Year <- 2018
+
+crime19$Year <- 2019
+
+pers_crime15 <- crime15 %>% filter(CrimeAgainst == "Person")
+
+pers_crime16 <- crime16 %>% filter(CrimeAgainst == "Person")
+
+pers_crime17 <- crime17 %>% filter(CrimeAgainst == "Person")
+
+pers_crime18 <- crime18 %>% filter(CrimeAgainst == "Person")
+
+pers_crime19 <- crime19 %>% filter(CrimeAgainst == "Person")
+
+allPersCrime <- bind_rows(pers_crime15, pers_crime16, pers_crime17, pers_crime18, pers_crime19)
