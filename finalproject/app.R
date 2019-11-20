9589feb507550a8895029e3725939e9b6b0f24f5
Sophia Freuden <sfreuden@.g.harvard.edu>
2019-11-19 21:19:58 -0500

    Functional version of my app with my first tab and interactive graphic! So happy I could cry.

--- finalproject/app.R
+++ finalproject/app.R
@@ -1,60 +1,65 @@
-#
-# This is a Shiny web application. You can run the application by clicking
-# the 'Run App' button above.
-#
-# Find out more about building applications with Shiny here:
-#
-#    http://shiny.rstudio.com/
-#
 
 library(shiny)
+library(readxl)
+library(lubridate)
 library(tidyverse)
 
 crime18 <- read_csv(file = "raw-data/CrimeData-2018.csv", col_types = cols(
-    Address = col_character(),
-    CaseNumber = col_character(),
-    CrimeAgainst = col_character(),
-    Neighborhood = col_character(),
-    OccurDate = col_character(),
-    OccurTime = col_character(),
-    OffenseCategory = col_character(),
-    OffenseType = col_character(),
-    OpenDataLat = col_double(),
-    OpenDataLon = col_double(),
-    OpenDataX = col_character(),
-    OpenDataY = col_character(),
-    ReportDate = col_character(),
-    OffenseCount = col_double()
+  Address = col_character(),
+  CaseNumber = col_character(),
+  CrimeAgainst = col_character(),
+  Neighborhood = col_character(),
+  OccurDate = col_character(),
+  OccurTime = col_character(),
+  OffenseCategory = col_character(),
+  OffenseType = col_character(),
+  OpenDataLat = col_double(),
+  OpenDataLon = col_double(),
+  OpenDataX = col_character(),
+  OpenDataY = col_character(),
+  ReportDate = col_character(),
+  OffenseCount = col_double()
 ))
 
 ui <- fluidPage(
-    navbarPage("Growing Pains in Portland: A Story of Crime, Unemployment, and Population",
-        tabPanel("Crime",
-            tabsetPanel(
-                tabPanel("Victims in 2018",
-            h3("Crime in 2018 by Victim Type"),
-            br(),
-            sidebarPanel(
-            selectInput(inputId = "victim",
-                         "Sample size", value = 25),
-            plotOutput(outputId = "hist") 
-        )))),
+  navbarPage(
+    "Growing Pains in Portland: A Story of Crime, Unemployment, and Population",
+    tabPanel(
+      "Crime",
+      tabsetPanel(
         tabPanel(
-            "About",
-            h4("About my project"),
-            "This is something about my project."
+          "Victims in 2018",
+          h3("Crime in 2018 by Victim Type"),
+          br(),
+          sidebarPanel(
+            selectInput(
+              inputId = "victim",
+              "Victim type:", choices = c("Person", "Society", "Property")
+            ),
+            h6("Crimes against property far outnumber crimes against people or society."),
+            h6("Crimes against property are also more diverse in type, though most are varying
+                   types of theft.")
+          )
+        ),
+        mainPanel(
+          plotOutput(outputId = "victimbar"),
+          br()
         )
+      )
     )
+  )
 )
 
 
 server <- function(input, output) {
-    output$img <- renderImage({
-        list(
-            src = "animation.gif",
-            contentType = "image/gif"
-        )
-    }, deleteFile = FALSE)
+  output$victimbar <- renderPlot({
+    crime18 %>%
+      filter(CrimeAgainst == input$victim) %>%
+      ggplot(aes(x = OffenseType)) +
+      geom_bar() +
+      coord_flip() +
+      scale_y_log10()
+  })
 }
 
 shinyApp(ui = ui, server = server)
